335e83f31392ff8bf3fa3fc63854c6f8a63f8996
Andrey Kumykov <aakumykov@yandex.ru>
2025-02-21 02:57:31 +0000

    Восстанавливается загрузочная запись

--- restore_inversed.sh
+++ restore_inversed.sh
@@ -23,15 +23,16 @@ show_as_error TARGET_DRIVE: $TARGET_DRIVE
 cat disks/sfdisk_dump | buffer | bsdtar -xf- -O sfdisk_dump | (sfdisk $TARGET_DRIVE) &
 SFDISK_PID=$!
 
-#cat disks/swap_info | bsdtar -xf- -O swap_info | (sleep 40 && IFS=: read INDEX UUID; sleep 3 && mkswap -U $UUID ${TARGET_DRIVE}${INDEX}) &
+# TODO: использовать отдельную команду с проверкой на наличие раздела
+#cat disks/swap_info | bsdtar -xf- -O swap_info | (sleep 2 && IFS=: read INDEX UUID; sleep 3 && mkswap -U $UUID ${TARGET_DRIVE}${INDEX}) &
 #SWAP_PID=$!
 
-#cat disks/boot_rec | bsdtar -xf- -O boot_rec | (sleep 40 && dd of=$TARGET_DRIVE bs=440 count=1) &
-#BOOT_PID=$!
+cat disks/boot_rec | bsdtar -xf- -O boot_rec | (dd of=$TARGET_DRIVE bs=440 count=1) &
+BOOT_PID=$!
 
-#cat archive | tee disks/sfdisk_dump | buffer | tee disks/swap_info | tee disks/boot_rec > /dev/null &
-#cat archive | tee disks/sfdisk_dump | buffer | tee disks/swap_info | tee disks/boot_rec > /dev/null &
-cat archive | tee disks/sfdisk_dump > /dev/null &
+#cat archive | tee disks/sfdisk_dump | tee disks/swap_info | tee disks/boot_rec > /dev/null &
+cat archive | tee disks/sfdisk_dump | tee disks/boot_rec > /dev/null &
+#cat archive | tee disks/sfdisk_dump > /dev/null &
 TEE_PID=$!
 
 cat /dev/stdin > archive 
