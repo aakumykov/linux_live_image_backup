24cd85062aa3969049b556980d1f815556a3be05
Andrey Kumykov <aakumykov@yandex.ru>
2025-03-25 12:57:01 +0000

    fix: работа скрипта install_dattobd_debian_ubuntu.sh

--- install_dattobd_debian_ubuntu.sh
+++ install_dattobd_debian_ubuntu.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-(which gpg /dev/null || which gpg2 > /dev/null ) || { 
+if !(which gpg > /dev/null) && !(which gpg2 > /dev/null); then
 	echo "gpg or gpg2 command required but not found!" > /dev/stderr
 	exit 1
-}
+fi
 
 sudo apt-key adv --fetch-keys https://cpkg.datto.com/DATTO-PKGS-GPG-KEY
 echo "deb [arch=amd64] https://cpkg.datto.com/datto-deb/public/$(lsb_release -sc) $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/datto-linux-agent.list
