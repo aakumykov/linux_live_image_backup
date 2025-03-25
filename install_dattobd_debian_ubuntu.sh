53c217d58a39a24a64764da52894897d83eb82e2
Andrey Kumykov <aakumykov@yandex.ru>
2025-03-25 12:55:30 +0000

    install_dattobd_debian_ubuntu.sh

---
+++ install_dattobd_debian_ubuntu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+(which gpg /dev/null || which gpg2 > /dev/null ) || { 
+	echo "gpg or gpg2 command required but not found!" > /dev/stderr
+	exit 1
+}
+
+sudo apt-key adv --fetch-keys https://cpkg.datto.com/DATTO-PKGS-GPG-KEY
+echo "deb [arch=amd64] https://cpkg.datto.com/datto-deb/public/$(lsb_release -sc) $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/datto-linux-agent.list
+sudo apt-get update
+sudo apt-get install dattobd-dkms dattobd-utils
