3e86fef749fac8fb46ffdbe6a24bc0ed6710fcb6
Andrey Kumykov <aakumykov@yandex.ru>
2025-03-26 09:43:49 +0000

    Добавил в install_dattobd_debian_ubuntu.sh проверку наличия команды dirmngr.

--- install_dattobd_debian_ubuntu.sh
+++ install_dattobd_debian_ubuntu.sh
@@ -5,6 +5,11 @@ if !(which gpg > /dev/null) && !(which gpg2 > /dev/null); then
 	exit 1
 fi
 
+which dirmngr || {
+	echo "dirmngr command required but not found!" > /dev/stderr
+	exit 1
+}
+
 sudo apt-key adv --fetch-keys https://cpkg.datto.com/DATTO-PKGS-GPG-KEY
 echo "deb [arch=amd64] https://cpkg.datto.com/datto-deb/public/$(lsb_release -sc) $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/datto-linux-agent.list
 sudo apt-get update
